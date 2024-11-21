51c6faa34c6f0b058a25530c529bef68884ac343
David <dlopmun1206@g.educaand.es>
2024-11-21 17:33:25 +0100

    Arreglos actividad 3

--- Actividades oficiales/Actividad 3/actividad3.py
+++ Actividades oficiales/Actividad 3/actividad3.py
@@ -1,5 +1,8 @@
 from libreria_actividad3 import crear_lista, seleccion, operaciones
 
+#Crear la lista de los números que se deseen añadir
 num = crear_lista()
+#Selecionar un tipo de operación
 opcion = seleccion()
+#Realizar la operación
 operaciones(num,opcion)
